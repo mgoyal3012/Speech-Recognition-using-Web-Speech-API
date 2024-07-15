41ec3e0ec2edf71a2e1bba577167678420e8340b
mgoyal3012 <meeragoyal30@gmail.com>
2024-07-15 14:00:48 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import VoiceInput from './components/voice-input'
+function App() {
+  return (
+    <div className="App">
+      <VoiceInput/>
+    </div>
+  );
+}
+
+export default App;
